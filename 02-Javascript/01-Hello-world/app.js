c43c37e65c3683622215ea4bcecd5d660bc02bb0
mabdullahjs <mabdullah2037@outlook.com>
2024-02-12 20:27:12 +0500

    js hello world

---
+++ 02-Javascript/01-Hello-world/app.js
@@ -0,0 +1,176 @@
+// console.log("hello world");
+
+// alert("hello world");
+
+
+// var 1first; xx
+
+// var firstName = "abdullah";
+// var FirstName = "abdullah";
+// console.log(firstName);
+
+
+// var firstName;
+// firstName = 'abdullah'
+
+// console.log(firstName);
+
+
+
+
+
+
+
+
+
+
+
+
+// data types
+
+//string "fdskfjdsk 2322 !#!@#@!$@!$!R%#@%#@/"
+//number 23
+//boolean true | false
+
+//object 
+
+
+
+
+
+
+
+// var firstName = true;
+// var firstName = "abdullah";
+// var firstName = 12;
+// console.log(typeof(firstName));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = "10";
+// var num2 = "20";
+// console.log(num1 + num2);
+// console.log(num1 + num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var username = prompt("enter your name");
+// console.log("hello " + username);
+// console.log(typeof(username));
+
+
+//my name is {firstname} {lastname} and my age is {age}
+
+
+
+
+
+
+
+
+var firstName = prompt('enter your firstname');
+var lastName = prompt('enter your lastname');
+var age = prompt('enter your age');
+
+console.log('my name is ' + firstName + " " + lastName + " and my age is " + age )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
