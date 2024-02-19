c3972a0ae53d6ed5e9f13c140957360be76b24b1
mabdullahjs <mabdullah2037@outlook.com>
2024-02-19 20:29:20 +0500

    functions

--- 02-Javascript/03-If-Else-Statement/app.js
+++ 02-Javascript/03-If-Else-Statement/app.js
@@ -1,4 +1,4 @@
-console.log('hello world');
+// console.log('hello world');
 // console.log('hello world1');
 // console.log('hello world2');
 // console.log('hello world3');
@@ -89,6 +89,143 @@ console.log('hello world');
 
 
 // user sa loga uski age| agar age uski 15 sa choti hogi to usko gym ma admission nahi milaiga agar uski age 15 sa 18 ka darmiyaan ma hogi to "500 zyada daa or aja gym maa" or jo 18 sa bara hoga usko gym ma admission milaiga. agar user ki age 50 sa uper hogi to usko bhi admission nahi dena ha
+
+
+// var age = +prompt('Enter your Age!');
+// console.log(typeof (age));
+
+// if (age <= 15) {
+//     console.log('Chalo shaba kato');
+// } else if (age < 18) {
+//     console.log('500 zyada daa or kisi ko btayo mat!')
+// } else {
+//     console.log('Welcome to my gym!')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var birthYear = +prompt('Enter your birth year!');
+// var currentYear = 2024;
+// console.log('Your age is ===>' , currentYear - birthYear);
+
+
+
+
+// var userName = 'abdu';
+// console.log(userName.length);
+
+// if(userName.length < 8){
+//     alert('Password 8 sa bara likh bhai!');
+// }else{
+//     console.log('ok ki report ha!')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Guess the number between 1 to 10
+
+// var secretNumber = 9;
+// var userInput = +prompt('Enter any number between 1 to 10');
+
+// if (userInput === secretNumber) {
+//     console.log('Jeet gaye bhai tum!');
+// } else if (userInput - 1 === secretNumber || userInput + 1 === secretNumber) {
+//     console.log('Maalik qareeb qareeb ho tum!')
+// } else {
+//     console.log('Haar gaye tum!');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 02-Javascript/04-Functions&Dom-Intro/app.js
@@ -0,0 +1,202 @@
+// console.log('hello world');
+// console.log('hello world1');
+// console.log('hello world2');
+// console.log('hello world3');
+
+
+// function greetUser() {
+//     console.log('hello world');
+//     console.log('hello world1');
+//     console.log('hello world2');
+//     console.log('hello world3');
+// }
+
+// greetUser();
+// greetUser();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function bnata waqt () ka undar jo bhi likhonga iska matlb function k undar parameter receive horha haa || function call krta waqt jo values bhejonga usko kahonga argument pass kr rha ho!
+
+
+// function sum(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// var abc = +prompt('Enter num1');
+// var xyz = +prompt('Enter num2');
+// sum(abc, xyz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createTable(num){
+//     console.log(num + " x 1 = " + num * 1);
+//     console.log(num + " x 2 = " + num * 2);
+//     console.log(num + " x 3 = " + num * 3);
+//     console.log(num + " x 4 = " + num * 4);
+//     console.log(num + " x 5 = " + num * 5);
+//     console.log(num + " x 6 = " + num * 6);
+//     console.log(num + " x 7 = " + num * 7);
+//     console.log(num + " x 8 = " + num * 8);
+//     console.log(num + " x 9 = " + num * 9);
+//     console.log(num + " x 10 = " + num * 10);
+// }
+
+// var numberForTable = +prompt('enter number for table!');
+// createTable(numberForTable)
+
+
+
+
+// var num = prompt('enter value');
+// console.log(num);
+
+
+// function sum(num1 , num2){
+//     // console.log(num1 + num2);
+//     var sumOfNumber = num1 + num2
+//     return sumOfNumber
+// }
+// var user = sum(10 , 20);
+// console.log('custom function===>' , user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function multiplyNumbers(num1, num2, num3) {
+//     return num1 * num2 * num3
+// }
+
+// var totalAmount = multiplyNumbers(10, 20, 30)
+// console.log('multiplication ===> ', totalAmount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function greetUser(num1 , num2){
+//     console.log(num1 + num2);
+//     return 'hello world'
+// }
+
+// greetUser(10 , 20);
+
+
+
+//local or global variable
+
+
+// var username = 'abdullah'
+
+// function greetUser(){
+//     var lastname = 'khan'
+//     console.log('Hello ' + username + ' ' + lastname);
+// }
+// console.log(lastname);
+// greetUser()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = null;
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
