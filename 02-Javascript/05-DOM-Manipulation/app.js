89526e620a1261fbc5cab6b1710039d8a47c87f9
mabdullahjs <mabdullah2037@outlook.com>
2024-02-27 20:26:04 +0500

    array

--- 02-Javascript/05-DOM-Manipulation/app.js
+++ 02-Javascript/05-DOM-Manipulation/app.js
@@ -41,14 +41,62 @@ console.log('hello world');
 var input = document.querySelector('.value');
 var head = document.querySelector('.head');
 
-function showVal(){
+function showVal() {
     console.log(input.value);
     head.innerHTML = input.value
 }
 
-function clearHead(){
+function clearHead() {
     head.innerHTML = ''
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 400
+// 4 form field bnaingi or 4 ma chem marks, phy,isl,urdu aik button hoga jaisa hi us button pa click hoga percentage nikaalka neecha h1 ma show krwani ha.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var physicsMarks = document.querySelector('#physics');
+var chemistryMarks = document.querySelector('#chemistry');
+var islamiatMarks = document.querySelector('#islamiat');
+var urduMarks = document.querySelector('#urdu');
+var obtainedspan = document.querySelector('#obtainedmarks');
+var percentagespan = document.querySelector('#percentage');
+var totalMarks = 400;
+
+
+function calculatePercentage (){
+    var obtainedMarks = +physicsMarks.value + +chemistryMarks.value + +islamiatMarks.value + +urduMarks.value
+    console.log(obtainedMarks)
+    obtainedspan.innerHTML = obtainedMarks;
+
+    var percentage = obtainedMarks / totalMarks * 100;
+    percentagespan.innerHTML = percentage + '%'
+} 
 
 
 
---
+++ 02-Javascript/06-Array/app.js
@@ -0,0 +1,110 @@
+// console.log('hello world');
+
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+
+
+// var cities = ['karachi' , 'lahore' , 'islamabad' , 'shikarpur' , 20 , true , ['macharcolony' , 'fb area']]
+
+// console.log(cities[0]);
+// console.log(cities[6][0]);
+// console.log(cities.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var foods = ['tikka' , 'roti' , 'salan' , 'chawal' , 'salad'];
+
+// console.log(foods);
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+// var cities = ['karachi' , 'lahore' , 'islamabad' , 'quetta' , 'hyderabad'];
+// cities[1] = 'larkana'
+// cities.push('larkana')
+// cities.pop();
+// cities.unshift('larkana')
+// cities.shift()
+// cities[2].push('karachi');
+
+// splice('starting index' , 'how many item should be deleted' , 'add item to that index')
+// cities.splice(3 , 1 , 'shikarpur' , 'rahimyarkhan' , 'bahawalpur');
+
+// var shrink = cities.slice(2 , 4);
+
+
+// console.log(cities)
+// console.log('slice ===>' , shrink)
+
+
+
+
+
+
+
+
+
+
+
+var city = Array('karachi'  , 'lahore');
+console.log(city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
