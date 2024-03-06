3b680ba4bb47ea95b1fccaafc73df68968a230e3
mabdullahjs <mabdullah2037@outlook.com>
2024-03-06 09:08:17 +0500

    loops

--- 02-Javascript/07-Loops/app.js
+++ 02-Javascript/07-Loops/app.js
@@ -104,18 +104,217 @@
 
 
 
-const fruit = ['apple' , 'banana' , 'orange' , 'watermelon' , 'strawberry' , 'melon' , 'grapes' , 'pineapple' , 'guava'];
-const select = document.querySelector('select');
+// const fruit = ['apple' , 'banana' , 'orange' , 'watermelon' , 'strawberry' , 'melon' , 'grapes' , 'pineapple' , 'guava'];
+// const select = document.querySelector('select');
 
-for (let i = 0; i < fruit.length; i++) {
-    console.log(fruit[i])
-    select.innerHTML += `<option>${fruit[i]}</option>`
-}
+// for (let i = 0; i < fruit.length; i++) {
+//     console.log(fruit[i])
+//     select.innerHTML += `<option>${fruit[i]}</option>`
+// }
 
 
 //backtick string
 //push and splice
 //loop , while and do while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i == 4){
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let i = 11
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let i = 11
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 10)
+
+
+
+
+
+
+
+
+
+
+
+// Write a program to display the message "Hello World" 5 times in your browser using for loop.
+
+//Write a program to print multiplication table of any number using for loop. Table number & length should be taken as an input from user.
+
+
+
+
+
+
+
+
+
+
+// const tableNumber = +prompt('enter table number');
+// const tableLength = +prompt('enter table length');
+
+// for (let i = 1; i <= tableLength; i++) {
+//     console.log(`${tableNumber} * ${i} = ${i*tableNumber}`)
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// const singleElement = document.querySelector('.single-item')
+// const index = document.querySelector('.index')
+
+
+// for(let i = 0; i < fruits.length; i++){
+//     singleElement.innerHTML += `<h1>${fruits[i]}</h1>`
+//     index.innerHTML += `<p>element at ${i} is ${fruits[i]}</p>`
+// }
+
+
+
+
+
+
+
+
+
+
+// const items = ["cake", "apple pie", "cookie", "chips", "patties"]
+// const userInput = prompt('enter item');
+
+// for (let i = 0; i < items.length; i++) {
+//     if(items[i] === userInput){
+//         console.log('han bhai available ha');
+//     }else{
+//         console.log('available nahi')
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
