5591028ca60fcadf5fcaba256e69641c2332238b
mabdullahjs <mabdullah2037@outlook.com>
2024-03-09 20:29:46 +0500

    strings methods

---
+++ 02-Javascript/09-String-Methods/app.js
@@ -0,0 +1,134 @@
+// console.log('hello world');
+
+// const city = prompt('enter your city');
+// if(city.toLowerCase() === 'karachi'){
+//     console.log('you live in my city')
+// }else{
+//     console.log('idhar ana ki ijazat nahi.');
+// }
+
+// const city = 'KARACHI';
+// console.log(city.toLowerCase());
+// const city = 'karachi';
+// console.log(city.toUpperCase());
+
+
+// let city = prompt('enter your city name!');
+// let cleanestCities = ['karachi', 'lahore', 'peshawar', 'quetta'];
+
+// if(cleanestCities.includes(city.toLowerCase())){
+//     console.log('if chal rha ha!')
+// }else{
+//     console.log('else chal rha ha!');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const city  = 'karachi';
+// console.log(city.length)
+// console.log(city[2])
+// console.log(city.charAt(4))
+// console.log(city.indexOf('z'))
+// console.log(city.slice(2 , 5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let message = "Ali and Sami are best and friends. They play cricket Ali and Sami and football Ali and Sami together.";
+
+// // console.log(message.replace('Ali' , 'Abdullah'));
+// // console.log(message.replaceAll('Ali' , 'Abdullah'));
+
+// const str = '   Hello, World!   ';
+// console.log(str.trim());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const username = 'Muhammad Abdullah Khan';
+
+// console.log(username.split('a'));
+
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector('#input');
+
+// function checkVal(){
+//     if(input.value.trim().length > 0){
+//         console.log(input.value);
+//     }else{
+//         console.log('empty form');
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
