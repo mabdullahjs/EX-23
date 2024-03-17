00d768b3a1401da794f3aee9cae78a12744b18bf
mabdullahjs <mabdullah2037@outlook.com>
2024-03-17 16:44:17 +0500

    objects

---
+++ 02-Javascript/12-Objects/app.js
@@ -0,0 +1,211 @@
+// console.log('hello objects');
+// let greetUser = function () {
+//     console.log('hello world');
+// }
+// greetUser()
+
+//collection of key and property is called object
+// const user = {
+//     name: 'Muhammad Abdullah',
+//     email: 'mabdullah2037@gmail.com',
+//     age: 20
+// }
+// // console.log(user.email);
+// // console.log(user["email"]);
+// user.name = 'Usman'
+// user.age = 18
+// user.isAvailable = true
+// user.greetUser = function () {
+//     console.log('Hello Usman');
+//     return `hello usman`
+// }
+// console.log(user.greetUser());
+
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const user = {
+//     name:'Muhammad Abdullah',
+//     age: 20,
+//     isLoggedIn: false,
+//     greetUser: function (){
+//         console.log(this);
+//     },
+//     fruits: ['apple' , 'banana' , 'orange' , {
+//         name:'Usman' , 
+//         fruits2: ['apple' , {
+//             name:'ammar'
+//         }]
+//     }]
+// }
+// console.log(user.fruits[3].fruits2[1].name);
+// user.greetUser()
+// user.name = 'usman'
+// user.greetUser = function (){
+
+// }
+// delete user.name
+// console.log(user);
+
+
+
+// window.alert()
+// window.prompt()
+
+// console.log(this);
+
+
+
+
+
+
+
+
+
+
+
+// function hello (index){
+//     console.log(index.innerHTML);
+// }
+
+// hello()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const div = document.querySelector('div');
+
+
+// let students = [
+//     { name: "Alice", age: 20, grade: "A" },
+//     { name: "Bob", age: 22, grade: "B" },
+//     { name: "Charlie", age: 21, grade: "C" }
+// ];
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i].name);
+//     div.innerHTML += `
+//     <h1>Name: ${students[i].name}</h1>
+//     <h1>Age: ${students[i].age}</h1>
+//     <hr/>
+//     `  
+// }
+
+
+
+
+
+// let nestedData = [
+//     {
+//         category: "Electronics",
+//         subcategories: [
+//             {
+//                 name: "Smartphones",
+//                 products: [
+//                     { brand: "Apple", model: "iPhone 13", price: 999 },
+//                     { brand: "Samsung", model: "Galaxy S21", price: 799 },
+//                     { brand: "Google", model: "Pixel 6", price: 699 }
+//                 ]
+//             },
+//             {
+//                 name: "Laptops",
+//                 products: [
+//                     { brand: "Apple", model: "MacBook Pro", price: 1499 },
+//                     { brand: "Dell", model: "XPS 13", price: 1299 },
+//                     { brand: "HP", model: "Spectre x360", price: 1199 }
+//                 ]
+//             }
+//         ]
+//     },
+//     {
+//         category: "Clothing",
+//         subcategories: [
+//             {
+//                 name: "Men's",
+//                 products: [
+//                     { brand: "Nike", item: "T-shirt", price: 25 },
+//                     { brand: "Adidas", item: "Joggers", price: 40 }
+//                 ]
+//             },
+//             {
+//                 name: "Women's",
+//                 products: [
+//                     { brand: "Zara", item: "Dress", price: 50 },
+//                     { brand: "H&M", item: "Jeans", price: 30 }
+//                 ]
+//             }
+//         ]
+//     }
+// ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//dress
+//xps20
+//price: 40
+
+
+
+
+
+
